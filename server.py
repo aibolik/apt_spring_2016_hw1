6cd7048cc9e8e74ee9805ddb747a644d462408d2
Aibol Kussain <kairkanuly@gmail.com>
2016-02-05 04:32:59 +0600

    feat: Implement currency getter from public.mig.kz(usd, eur, rub)

--- server.py
+++ server.py
@@ -6,7 +6,26 @@ import os
 
 @route('/')
 def index():
-    return {"usd": 378, "eur": 415, "rub": 4.82}
+
+	URL = "http://public.mig.kz/"
+
+	resp = requests.get(URL)
+	bs4 = BeautifulSoup(resp.content, "html.parser")
+	rub = None
+	usd = None
+	eur = None
+	for tag in bs4.find_all('h4'):
+		if tag.string == 'usd':
+			p = tag.parent.p.string
+			usd = p.split(' ')[0]
+		if tag.string == 'rub':
+			p = tag.parent.p.string
+			rub = p.split(' ')[0]
+		if tag.string == 'eur':
+			p = tag.parent.p.string
+			eur = p.split(' ')[0]
+
+	return {"usd": usd, "eur": eur, "rub": rub}
 
 
 URL = "https://github.com/giAtSDU/apt_spring_2016_hw1"
